da309a4a03da9ec1bc1b5a08708866b67aa2d53b
Dan Pollard <d.j.pollard@sheffield.ac.uk>
2023-06-20 14:33:56 +0100

    added the licence details to the start of all code files

--- R/generate_random.R
+++ R/generate_random.R
@@ -1,3 +1,25 @@
+#    Embedding RCT Health Economic Analysis using the Sheffield Type 2 Diabetes Treatment Model - version 3
+#    Copyright (C) 2023  Pollard,Pidd,Breeze,Brennan
+
+#    This program is free software; you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation; either version 2 of the License, or
+#    (at your option) any later version.
+
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+
+#    You should have received a copy of the GNU General Public License along
+#    with this program; if not, write to the Free Software Foundation, Inc.,
+#    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+#    Contact person: Dan Pollard, Email: d.j.pollard@sheffield.ac.uk, 
+#    Address: Regent Court, 30 Regent Court, Sheffield, United Kingdom, S1 4DA
+
+
+
 ### Set up random matrix array
 ##'@param n is the number of patients in this model run
 ##'@return random_numbers is an array of random numbers for all possible events 
